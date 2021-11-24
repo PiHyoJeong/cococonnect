68a98ac4a520f122638b5773c00369af2d7b85c3
PiHyoJeong <78262975+PiHyoJeong@users.noreply.github.com>
2021-11-25 03:24:35 +0900

    change web

--- web.js
+++ web.js
@@ -1,22 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { cors: { origin: "*" } });
+var loginmember='2134234';
 
-app.get('/', function (req, res) {
+app.get('/', function (_req, res) {
     res.send('<h1>안녕하세요 "/" 경로 입니다.</h1>');
 });
 
 io.on('connection', function (socket) {
-    console.log('한명의 유저가 접속을 했습니다.');
-    socket.on('disconnect', function () {
-        console.log('한명의 유저가 접속해제를 했습니다.');
-    });
+    socket.on('newuser', function(nick){
+        var newUser = nick;
+        console.log('한명의 유저가 접속을 했습니다.');
 
-    socket.on('send_msg', function (msg) {
-        //콘솔로 출력을 한다.
-        console.log(msg);
-        //다시, 소켓을 통해 이벤트를 전송한다.
-        io.emit('send_msg', msg);
+        socket.on('disconnect', function () {
+            console.log('1//0//'+newUser);
+            io.emit('send_msg', '1//0//'+newUser);
+        });
+        socket.on('send_msg', function (msg) {
+            //콘솔로 출력을 한다
+            console.log(msg);
+            //다시, 소켓을 통해 이벤트를 전송한다.
+            io.emit('send_msg', msg);
+        });
     });
 });
 
